ea7a33b7584aabb5ade545f9769ec1cca535307e
Aleksey Pekhterev <qudaeo@yandex.ru>
2025-02-13 13:03:14 +0700

    feat: convert to functional component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, type FC } from 'react';
+import React, { type FC, useState } from 'react'
 import {
   StyleSheet,
   ScrollView,
@@ -10,9 +10,8 @@ import {
 } from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-//import SheetMenu from 'react-native-sheetmenu'
 
-import YandexPayment from '../../';
+import YandexPayment, { PaymentType, Currency } from '../../'
 import SwitchView from './components/SwitchView';
 import config from './config';
 
@@ -41,184 +40,175 @@ const Button: FC<ButtonProps> = ({ onPress, style, title }) => {
   );
 };
 
-class App extends Component {
-  state = {
-    paymentTypes: {
-      YOO_MONEY: null,
-      GOOGLE_PAY: null,
-      BANK_CARD: null,
-      SBERBANK: null,
-      SBP: null,
-    },
-    currency: 'RUB',
+const App = () => {
+  const [paymentTypes, setPaymentTypes] = useState<PaymentType[]>([]);
+  const [currency, _] = useState<Currency>('RUB');
+
+  const changePaymentType = (paymentType: PaymentType) => {
+    if (paymentTypes.includes(paymentType)) {
+      setPaymentTypes(paymentTypes =>
+        paymentTypes.filter(p => p !== paymentType)
+      )
+    } else {
+      setPaymentTypes(paymentTypes =>
+        [...paymentTypes, paymentType]
+      )
+    }
   };
 
-  changePaymentType = (checked: any, code: any) => {
-    const paymentTypes: any = this.state.paymentTypes;
-    paymentTypes[code] = checked ? code : null;
-    this.setState({ paymentTypes });
-  };
+  // const onSelectCurrency = (currency: any) => {
+  //   setState({ ...state, currency });
+  // };
 
-  onSelectCurrency = (currency: any) => {
-    this.setState({
-      currency: currency,
-    });
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <View>
-            <SwitchView
-              title="BANK_CARD"
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 8,
-                marginVertical: 1,
-                paddingHorizontal: 16,
-              }}
-              checked={!!this.state.paymentTypes['BANK_CARD']}
-              onChanges={(checked) => {
-                this.changePaymentType(checked, 'BANK_CARD');
-              }}
-            />
-
-            <SwitchView
-              title="YOO_MONEY"
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 8,
-                marginVertical: 1,
-                paddingHorizontal: 16,
-              }}
-              checked={!!this.state.paymentTypes['YOO_MONEY']}
-              onChanges={(checked) => {
-                this.changePaymentType(checked, 'YOO_MONEY');
-              }}
-            />
-
-            <SwitchView
-              title="SBERBANK"
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 8,
-                marginVertical: 1,
-                paddingHorizontal: 16,
-              }}
-              checked={!!this.state.paymentTypes['SBERBANK']}
-              onChanges={(checked) => {
-                this.changePaymentType(checked, 'SBERBANK');
-              }}
-            />
-
-            <SwitchView
-              title="GOOGLE_PAY"
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 8,
-                marginVertical: 1,
-                paddingHorizontal: 16,
-              }}
-              checked={!!this.state.paymentTypes['GOOGLE_PAY']}
-              onChanges={(checked) => {
-                this.changePaymentType(checked, 'GOOGLE_PAY');
-              }}
-            />
-
-            <SwitchView
-              title="SBP"
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 8,
-                marginVertical: 1,
-                paddingHorizontal: 16,
-              }}
-              checked={!!this.state.paymentTypes['SBP']}
-              onChanges={(checked) => {
-                this.changePaymentType(checked, 'SBP');
-              }}
-            />
-
-            <View
-              style={{
-                backgroundColor: '#fff',
-                paddingVertical: 16,
-                marginVertical: 16,
-                paddingHorizontal: 16,
-              }}
-            >
-              <TouchableOpacity
-                style={{ flexDirection: 'row' }}
-                onPress={() => {
-                  // new SheetMenu({
-                  //   title: 'Select currency:',
-                  //   actions: [
-                  //     {
-                  //       title: 'RUB',
-                  //       onPress: () => this.onSelectCurrency('RUB'),
-                  //     },
-                  //     {
-                  //       title: 'EUR',
-                  //       onPress: () => this.onSelectCurrency('EUR'),
-                  //     },
-                  //     {
-                  //       title: 'USD',
-                  //       onPress: () => this.onSelectCurrency('USD'),
-                  //     },
-                  //   ],
-                  // }).show()
-                }}
-              >
-                <Text style={{ flexGrow: 1 }}>Currency</Text>
-                <Text>{this.state.currency}</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-
-          {/* <Text style={{marginTop: 45, color: "#000"}}>{JSON.stringify(Object.values(this.state.paymentTypes).filter(it => it !== null))}</Text> */}
-
-          <Button
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}
+      >
+        <View>
+          <SwitchView
+            title="BANK_CARD"
             style={{
-              marginTop: 100,
-              backgroundColor: '#ffcc00',
-              borderRadius: 8,
+              backgroundColor: '#fff',
+              paddingVertical: 8,
+              marginVertical: 1,
+              paddingHorizontal: 16,
             }}
-            title="YandexPayment.show()"
-            onPress={async () => {
-              try {
-                const result = await YandexPayment.show(
-                  {
-                    id: config.id,
-                    token: config.token,
-                    name: 'React shop',
-                    description: `Buy on ${Platform.OS} ${Platform.Version}`,
-                  },
-                  {
-                    amount: 1.01,
-                    // @ts-ignore
-                    currency: this.state.currency,
-                    // @ts-ignore
-                    types: Object.values(this.state.paymentTypes).filter(
-                      (it) => it !== null
-                    ),
-                    yooKassaClientId: config.id,
-                  }
-                );
-                YandexPayment.close();
-                console.warn(JSON.stringify(result));
-              } catch (e) {
-                console.error(e);
-              }
+            checked={paymentTypes.includes('BANK_CARD')}
+            onChanges={() => {
+              changePaymentType('BANK_CARD');
             }}
           />
-        </ScrollView>
-      </View>
-    );
-  }
+
+          <SwitchView
+            title="YOO_MONEY"
+            style={{
+              backgroundColor: '#fff',
+              paddingVertical: 8,
+              marginVertical: 1,
+              paddingHorizontal: 16,
+            }}
+            checked={paymentTypes.includes('YOO_MONEY')}
+            onChanges={() => {
+              changePaymentType('YOO_MONEY');
+            }}
+          />
+
+          <SwitchView
+            title="SBERBANK"
+            style={{
+              backgroundColor: '#fff',
+              paddingVertical: 8,
+              marginVertical: 1,
+              paddingHorizontal: 16,
+            }}
+            checked={paymentTypes.includes('SBERBANK')}
+            onChanges={() => {
+              changePaymentType('SBERBANK');
+            }}
+          />
+
+          <SwitchView
+            title="GOOGLE_PAY"
+            style={{
+              backgroundColor: '#fff',
+              paddingVertical: 8,
+              marginVertical: 1,
+              paddingHorizontal: 16,
+            }}
+            checked={paymentTypes.includes('GOOGLE_PAY')}
+            onChanges={() => {
+              changePaymentType('GOOGLE_PAY');
+            }}
+          />
+
+          <SwitchView
+            title="SBP"
+            style={{
+              backgroundColor: '#fff',
+              paddingVertical: 8,
+              marginVertical: 1,
+              paddingHorizontal: 16,
+            }}
+            checked={paymentTypes.includes('SBP')}
+            onChanges={() => {
+              changePaymentType('SBP');
+            }}
+          />
+
+          <View
+            style={{
+              backgroundColor: '#fff',
+              paddingVertical: 16,
+              marginVertical: 16,
+              paddingHorizontal: 16,
+            }}
+          >
+            <TouchableOpacity
+              style={{ flexDirection: 'row' }}
+              onPress={() => {
+                // new SheetMenu({
+                //   title: 'Select currency:',
+                //   actions: [
+                //     {
+                //       title: 'RUB',
+                //       onPress: () => setCurrency('RUB'),
+                //     },
+                //     {
+                //       title: 'EUR',
+                //       onPress: () => setCurrency('EUR'),
+                //     },
+                //     {
+                //       title: 'USD',
+                //       onPress: () => setCurrency('USD'),
+                //     },
+                //   ],
+                // }).show()
+              }}
+            >
+              <Text style={{ flexGrow: 1 }}>Currency</Text>
+              <Text>{currency}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* <Text style={{marginTop: 45, color: "#000"}}>{JSON.stringify(Object.values(state.paymentTypes).filter(it => it !== null))}</Text> */}
+
+        <Button
+          style={{
+            marginTop: 100,
+            backgroundColor: '#ffcc00',
+            borderRadius: 8,
+          }}
+          title="YandexPayment.show()"
+          onPress={async () => {
+            try {
+              const result = await YandexPayment.show(
+                {
+                  id: config.id,
+                  token: config.token,
+                  name: 'React shop',
+                  description: `Buy on ${Platform.OS} ${Platform.Version}`,
+                },
+                {
+                  amount: 1.01,
+                  currency,
+                  types: paymentTypes,
+                  yooKassaClientId: 'clientId',
+                  savePaymentMethod: 'OFF'
+                }
+              );
+              YandexPayment.close();
+              console.warn(JSON.stringify(result));
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+        />
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
